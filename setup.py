95f454963ddde8a5882e32dc3c995cfa0b0baca9
0xMihalich <bayan-mobile@list.ru>
2024-10-21 23:13:04 +1000

    Fix: python setup.py sdist auto add requirements.txt

--- setup.py
+++ setup.py
@@ -1,19 +1,9 @@
 import shutil
 from setuptools import setup, find_packages
-import pathlib
-import pkg_resources
 
 
 shutil.rmtree("build", ignore_errors=True,)
 shutil.rmtree("clickhttp.egg-info", ignore_errors=True,)
-
-
-with pathlib.Path('requirements.txt').open() as requirements_txt:
-    install_requires = [
-        str(requirement)
-        for requirement
-        in pkg_resources.parse_requirements(requirements_txt)
-    ]
 
 with open(file="README.md", mode="r", encoding="utf-8",) as f:
     long_description = f.read()
@@ -21,7 +11,6 @@ with open(file="README.md", mode="r", encoding="utf-8",) as f:
 setup(name="clickhttp",
       version="0.1.1",
       packages=find_packages(),
-      install_requires=install_requires,
       author="0xMihalich",
       author_email="bayanmobile87@gmail.com",
       description="Работа с БД Clickhouse по HTTP-протоколу",
